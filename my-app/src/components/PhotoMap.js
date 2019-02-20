5572a9ce2b7c4958ca175e0545e60d3e845f16e8
sarfkermali <sarfkermali@gmail.com>
2019-02-20 12:21:50 -0700

    added google-maps-react and created Map component

---
+++ my-app/src/components/PhotoMap.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PhotoThumb from './PhotoThumb';
+
+class PhotoMap extends Component {
+    state = {  }
+    
+    render() { 
+        return (  );
+    }
+
+}
+ 
+export default PhotoMap;
+
+
+
+
+</PhotoThumb />
+<h1>Title</h1>
+<h2>City, Country</h2>
+<button>View</button>
+<button>Edit</button>
