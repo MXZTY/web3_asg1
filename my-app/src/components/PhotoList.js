c9dec16478a4c543db7b4e2a71b48a22936c6145
sarfkermali <sarfkermali@gmail.com>
2019-02-23 21:40:56 -0700

    implemented country and city filter in PhotoList Component

--- my-app/src/components/PhotoList.js
+++ my-app/src/components/PhotoList.js
@@ -2,21 +2,50 @@ import React from "react";
 import PhotoThumb from "./PhotoThumb.js";
 
 class PhotoList extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      search:'',//search term
+      input:''//either country or city
+    }
+  }
+
+  updateSearch=(e)=>{
+    //if country changed
+    if(e.target.id==="countryInput"){
+      this.setState({search: e.target.value, input:"Country"})
+    }else{//city changed
+      this.setState({search: e.target.value, input:"City"})
+    }
+  }
 
   render() {
+    //https://www.youtube.com/watch?v=OlVkYnVXPl0
+    //returns an array filtered based on the users search term (stored in state)
+    let filteredList = this.props.photos.filter(
+      (photo)=>{
+        //if city changed
+        if(this.state.input==="City"){
+          return photo.city.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        }else{//country changed
+          return photo.country.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        }
+      } 
+    )
     if (this.props.photos.length > 1) {
       return (
         <div className="flex-container">
           <div className="filterBy flex-box">
            <h2 className="flex-title"> Filter By</h2>
-             <input id="countryInput" className="flex-item" name="filterCountry" type="text" placeholder="Country" onChange={this.props.filterPhotos}/> 
-             <input id="cityInput" className="flex-item" name="filterCity" type="text" placeholder="City" onChange={this.props.filterPhotos}/>
+             <input id="countryInput" className="flex-item" name="filterCountry" type="text" placeholder="Country" onChange={this.updateSearch}/> 
+             <input id="cityInput" className="flex-item" name="filterCity" type="text" placeholder="City" onChange={this.updateSearch}/>
           </div>
             
 
         <article className="photos">
 
-          {this.props.photos.map(p => (
+          {filteredList.map(p => (
             <PhotoThumb
               photo={p}
               key={p.id}
